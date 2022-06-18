76cedbe90c59bf95a457f7dbfda376095bd9fc32
DiAlPiRo <diegop.rosero@gmail.com>
2022-06-18 09:15:22 -0500

    fin del ejercicio

--- src/actividad2.java
+++ src/actividad2.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class actividad2 {
     public static void main(String args[]) {
-int pesos;
-    }
+int pesos = Integer.parseInt(JOptionPane.showInputDialog("ingresar la cantidad de pesos"));
+double bolivar = (0.0014 * pesos);
+        JOptionPane.showMessageDialog(null,"el valor de pesos en bolivares es" + bolivar);
+
+ }
 }
